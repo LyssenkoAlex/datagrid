0ef66282aea36faddb438c9277442ce64ef06ade
ALEX\alex <lyssenko.alex@gmail.com>
2020-03-08 02:26:31 +0600

    init: basic package

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <React.Fragment>
+            <div  >
+               world
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default App;
