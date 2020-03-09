b7407c7d4bdc057a77a6d0d61c77de8a39115de6
ALEX\alex <lyssenko.alex@gmail.com>
2020-03-10 00:04:33 +0600

    feat: table

--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -1,20 +1,37 @@
 
 import React, {useState} from 'react';
 import { useDispatch, useSelector} from 'react-redux'
+import  '../style/style.scss'
 
 
 export default function CustomTable() {
 
     const tableData = useSelector(state => state.tableData);
 
-    let cells = tableData.map((row, index) =>
-        <span key={index}>{row.Respondent}</span>
+    let cells = tableData.slice(0,300).map((row, index) =>
+        <tr key={`row_${index}`}>
+            <td key={`age_${index}`}>{row.Age}</td>
+            <td key={`Country_${index}`}>{row.Country}</td>
+            <td key={`Employment_${index}`}>{row.Employment}</td>
+            <td key={`Gender_${index}`}>{row.Gender}</td>
+            <td key={`Hobbyist_${index}`}>{row.Hobbyist}</td>
+            <td key={`LanguageWorkedWith_${index}`}>{row.LanguageWorkedWith}</td>
+            <td key={`MainBranch_${index}`}>{row.MainBranch}</td>
+            <td key={`OpSys_${index}`}>{row.OpSys}</td>
+            <td key={`Respondent_${index}`}>{row.Respondent}</td>
+            <td key={`Student_${index}`}>{row.Student}</td>
+            <td key={`SurveyLength_${index}`}>{row.SurveyLength}</td>
+            <td key={`WebFrameWorkedWith_${index}`}>{row.WebFrameWorkedWith}</td>
+            <td key={`YearsCode_${index}`}>{row.YearsCode}</td>
+        </tr>
     );
 
     return (
-        <div>
+        <table>
+            <tbody>
             {cells}
-        </div>
+            </tbody>
+        </table>
     )
 
 }
--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -2,11 +2,13 @@ import {
     SORT,
 } from "../actions/actions";
 import rowData from '../../data/data_source';
+import PropTypes from 'prop-types';
 
 
 
 const initialState = {
     tableData:rowData,
+    rowsPerPage:50,
 };
 
 
@@ -22,3 +24,21 @@ function directorsRootReducer(state = initialState, action) {
 }
 
 export default directorsRootReducer;
+
+directorsRootReducer.propTypes = {
+    tableData: PropTypes.shape({
+        Age: PropTypes.number.isRequired,
+        Country: PropTypes.string.isRequired,
+        Employment: PropTypes.string.isRequired,
+        Gender: PropTypes.string.isRequired,
+        Hobbyist: PropTypes.string.isRequired,
+        LanguageWorkedWith: PropTypes.string.isRequired,
+        MainBranch: PropTypes.string.isRequired,
+        OpSys: PropTypes.string.isRequired,
+        Respondent: PropTypes.number.isRequired,
+        Student: PropTypes.string.isRequired,
+        SurveyLength: PropTypes.string.isRequired,
+        WebFrameWorkedWith: PropTypes.string.isRequired,
+        YearsCode: PropTypes.number.isRequired
+    }).isRequired
+};
